e2e86ec0019b3cee868a9a802d5613d2ba639dfc
gojicmiroslav <miroslavy2k@gmail.com>
2015-11-24 01:46:01 +0100

    Finish layout

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe ApplicationHelper, :type => :helper do
 	
 	describe "#full_title" do
 		it "should render full title" do
-			expect(helper.full_title).to eq("MG Social Site")
-			expect(helper.full_title("Help")).to eq("Help | MG Social Site")
+			expect(helper.full_title).to eq("MG Advertisement")
+			expect(helper.full_title("Help")).to eq("Help | MG Advertisement")
 		end
 	end
 end
