744bb048eb76c985d58b8195f1d5161647821ed5
gojicmiroslav <miroslavy2k@gmail.com>
2015-11-26 15:57:31 +0100

    Finish user register:   - added confimable but not tested   - added tests for valid and invalid data

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'static_pages#home'
+  get  'help'   => 'static_pages#help'
+  get  'about'   => 'static_pages#about'
+  get  'contact' => 'static_pages#contact'
 
-  get 'help'   => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
+  devise_for :users
+  resources :users, only: :show
 
 end
