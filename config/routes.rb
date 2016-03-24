3eff00bab2b10e784fe4a0323276a1350fa7e248
gojicmiroslav <miroslavy2k@gmail.com>
2016-03-24 23:24:04 +0100

    Add Pagination

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
-  root 'categories#index'
+  root 'static_pages#home'
+
   get  'help'   => 'static_pages#help'
   get  'about'   => 'static_pages#about'
   get  'contact' => 'static_pages#contact'
